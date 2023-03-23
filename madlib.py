ad13a5b160a960af79feeac993aea05390390ce4
Dominick Martin <dominickbmartin@gmail.com>
2023-03-23 02:41:11 +0000

    lab 03 start

---
+++ madlib.py
@@ -0,0 +1,37 @@
+import os
+import re
+
+
+def get_madlibs_from_user(words_list)
+
+words =[]
+
+I the {Adjective} and {Adjective} {A First Name} have {Past Tense Verb} {A First Name}'s {Adjective} sister and plan to steal her {Adjective} {Plural Noun}!
+
+
+
+
+
+
+
+
+
+def print_welcome_message():
+    message = '''
+    Welcome to the Madlib Command Line Tool!
+
+    We'll guide you through the process of creating a fun Madlib story.
+    First, we'll read a template Madlib file, then prompt you to enter words
+    to fill in the blanks. Finally, we'll present the completed story and save it
+    to a new file in the current directory.
+    '''
+    print(message)
+
+
+
+
+
+
+
+
+
